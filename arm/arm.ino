d8f9899cdf1d2b045ae27db2cb1518b82e4b7f66
Michael Yang <mchaelyang@gmail.com>
2014-12-29 15:11:47 -0500

    added wrist servo to arduino code

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,35 +1,45 @@
 #include <Servo.h>
 
 Servo pinchServo;
+Servo wristServo;
 
-int numServos = 1;
+int numServos = 2;
 
 void setup() {
   Serial.begin(9600);
 
   pinchServo.attach(13);
+  wristServo.attach(12);
+
   pinchServo.write(160);
+  wristServo.write(90);
 }
+
+
+boolean reading = false;
+int count = 0;
 
 void loop() {
   if (Serial.available()) {
+    if (reading) {
+      if (Serial.available() >= numServos) {
+          byte data[numServos];
+          Serial.readBytes(data, numServos);
+          reading = false;
+          
+          pinchServo.write(data[0]);
+          wristServo.write(data[1]);
+      }
+    }
+    else {
+      byte first = Serial.read();
+      Serial.print("first: ");
+      Serial.println(first);
 
-    byte first = Serial.read();
-    Serial.print("first: ");
-    Serial.println(first);
-
-    if (first == 0xff) {
-      int count = 0;
-      while (count < numServos) {
-        if (Serial.available()) {
-          int pinch = Serial.read();// - '0';
-          //          Serial.print("pinch: ");
-          //          Serial.println(pinch);
-          count++;
-          pinchServo.write(pinch);
-        }
+      if (first == 0xff) {
+        reading = true;
       }
     }
   }
-  //  delay(5);
+
 }
