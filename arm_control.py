e0e57761431cc4c4ebd44d30d1987dad57c5edc7
Michael Yang <mchaelyang@gmail.com>
2014-12-29 17:33:45 -0500

    added rotation calculations, added limits to leap interaction

--- arm_control.py
+++ arm_control.py
@@ -1,4 +1,4 @@
-import sys, time
+import sys, time, math
 
 sys.path.insert(0, "../lib")
 import serial, Leap
@@ -6,11 +6,13 @@ import serial, Leap
 servo_bounds = {
 	'pinch': (90, 180),
 	'wrist': (0,180),
+	'twist': (10,180),
 }
 
 leap_bounds = {
 	'pinch': (0, 1),
 	'wrist': (-90,90),
+
 }
 
 class Listener(Leap.Listener):
@@ -33,7 +35,34 @@ class Listener(Leap.Listener):
 			hand = frame.hands.rightmost
 			pinch = map_range_tuples(leap_bounds['pinch'], servo_bounds['pinch'], 1 - hand.pinch_strength) # fix to use tuples later
 			wrist = map_range_tuples(leap_bounds['wrist'], servo_bounds['wrist'], hand.direction.pitch * Leap.RAD_TO_DEG)
-			# 1 - pinchStrength because servo is inverted
+			# 1 - pinchStrength because sersvo is inverted
+
+			x = hand.palm_position.x
+			y = hand.palm_position.y
+			z = hand.palm_position.z
+
+			# set bounds of interaction
+			if y < 150:
+				y = 150
+			if y > 500:
+				y = 500
+			if x > 200:
+				x = 200
+			if x < -200:
+				x = -200
+			if z > 200:
+				z = 200
+			if z < -200:
+				z = -200
+
+			z = 200 - z
+			rotation =  math.atan(z / x) * (180 / math.pi)
+			# print x, z, y
+			if x < 0:
+				rotation = 180 + rotation
+			rotation = 180 - rotation
+			print rotation
+
 			send = [255]
 			send.append(int(pinch))
 			send.append(int(wrist))
@@ -42,7 +71,8 @@ class Listener(Leap.Listener):
 			# for i in range(0, 90 - int(pinch - 90)):
 				# sys.stdout.write('-')
 			# print ''
-			print wrist
+			# print wrist
+
 			# clear buffers!
 			arduino_ser.flushInput()
 			arduino_ser.flushOutput()
