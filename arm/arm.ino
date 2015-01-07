3851bf7f847ba8a4425e08645a6240e6939ee282
ludannyhongyu@gmail.com <ludannyhongyu@gmail.com>
2015-01-06 22:39:39 -0500

    added smoothness to initialization and movements, needs to be confirmed and tested

--- arm/arm.ino
+++ arm/arm.ino
@@ -9,6 +9,13 @@ Servo twistServo;
 
 
 int numServos = 6;
+int pinch = 160;
+int wrist = 90;
+int elbow = 30;
+int shoulderL = 60;
+int shoulderR = 180 - 66;
+int twist = 95;
+int smoothness = 3; //change this for smoothness and speed of servos     added for less twitch in initialization
 
 void setup() {
   Serial.begin(9600);
@@ -20,12 +27,12 @@ void setup() {
   shoulderServoRight.attach(9);
   twistServo.attach(8);
 
-  pinchServo.write(160);
-  wristServo.write(90);
-  elbowServo.write(30);
-  shoulderServoLeft.write(60);
-  shoulderServoRight.write(180 - 66);
-  twistServo.write(95);
+  pinchServo.write(pinch);
+  wristServo.write(wrist);
+  elbowServo.write(elbow);
+  shoulderServoLeft.write(shoulderL);
+  shoulderServoRight.write(shoulderR);
+  twistServo.write(twist);
 }
 
 
@@ -39,12 +46,72 @@ void loop() {
         byte data[numServos];
         Serial.readBytes(data, numServos);
         reading = false;
-        pinchServo.write(data[0]);
-        wristServo.write(data[1]);
-        twistServo.write(data[2]);
-        elbowServo.write(data[3]);
-        shoulderServoLeft.write(data[4]);
-        shoulderServoRight.write(data[5]);
+        if(data[0]>pinch+smoothness){                 //can be shortened    added for smoothness
+          pinchServo.write(pinch+smoothness);
+          pinch = pinch+smoothness;
+        }
+        else if(data[0]<pinch-smoothness){
+          pinchServo.write(pinch-smoothness);
+          pinch = pinch-smoothness;
+        }
+        else{
+          pinchServo.write(pinch);
+        }
+        if(data[1]>wrist+smoothness){
+          wristServo.write(wrist+smoothness);
+          wrist = wrist+smoothness;
+        }
+        else if(data[1]<wrist-smoothness){
+          wristServo.write(wrist-smoothness);
+          wrist = wrist-smoothness;
+        }
+        else{
+          wristServo.write(wrist);
+        }
+        if(data[2]>twist+smoothness){
+          twistServo.write(twist+smoothness);
+          twist = twist+smoothness;
+        }
+        else if(data[2]<twist-smoothness){
+          twistServo.write(twist-smoothness);
+          twist = twist-smoothness;
+        }
+        else{
+          twistServo.write(twist);
+        }
+        if(data[3]>elbow+smoothness){
+          elbowServo.write(elbow+smoothness);
+          elbow = elbow+smoothness;
+        }
+        else if(data[3]<elbow-smoothness){
+          elbowServo.write(elbow-smoothness);
+          elbow = elbow-smoothness;
+        }
+        else{
+          elbowServo.write(elbow);
+        }
+        if(data[4]>shoulderL+smoothness){
+          shoulderServoLeft.write(shoulderL+smoothness);
+          shoulderL = shoulderL+smoothness;
+        }
+        else if(data[4]<shoulderL-smoothness){
+          shoulderServoLeft.write(shoulderL-smoothness);
+          shoulderL = shoulderL-smoothness;
+        }
+        else{
+          shoulderServoLeft.write(shoulderL);
+        }
+        if(data[5]>shoulderR+smoothness){
+          shoulderServoRight.write(shoulderR+smoothness);
+          shoulderR = shoulderR+smoothness;
+        }
+        else if(data[5]<shoulderR-smoothness){
+          shoulderServoRight.write(shoulderR-smoothness);
+          shoulderR = shoulderR-smoothness;
+        }
+        else{
+          shoulderServoRight.write(shoulderR);
+        }
       }
     }
     else {
